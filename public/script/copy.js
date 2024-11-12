2c34c9ab9326d09a3c4bb18634edae09fc9cadac
Muhammad Ranju <mdranju23@gmail.com>
2024-11-12 11:52:39 +0600

    Fix: Fix copy to clipboard functionality and layout
    * Fix copy button functionality
    * Fix previous short url copy
    * Fix recently list
    * Update modal and copy js
    * Update alert position

--- public/script/copy.js
+++ public/script/copy.js
@@ -1,50 +1,67 @@
-$(document).ready(function () {
-  "use strict";
+// $(document).ready(function () {
+//   "use strict";
 
-  $(window).keydown(function (event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      return false;
+//   $(window).keydown(function (event) {
+//     if (event.keyCode === 13) {
+//       event.preventDefault();
+//       return false;
+//     }
+//   });
+
+//   $.fn.shortlink({
+//     // ajax: 'https://short.php8developer.com/ajax/short',
+//     // siteUrl: 'https://short.php8developer.com',
+//     qr: "#qr",
+//     copy: "#copy",
+//     shortlink: "#shortlink",
+//     url: "#url",
+//     create: "#create",
+//     form: "#shortForm",
+//     shortarea: "#short-area",
+//     notice: "#copy-notice",
+//     share: ".share42init",
+//   });
+// });
+
+function copyToClipboard() {
+  const copyAlert = document.getElementById("copyAlert");
+  // Get the text field
+  const previousShortValue = document.getElementById("previousShortValue");
+  console.log(copyAlert);
+  // Select the text field
+  previousShortValue.select();
+  previousShortValue.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(previousShortValue.value).then(
+    function () {
+      copyAlert.classList.remove("hidden");
+      setTimeout(() => {
+        copyAlert.classList.add("hidden");
+      }, 1000);
+    },
+    function (err) {
+      console.error("Could not copy text: ", err);
     }
-  });
-
-  $.fn.shortlink({
-    // ajax: 'https://short.php8developer.com/ajax/short',
-    // siteUrl: 'https://short.php8developer.com',
-    qr: "#qr",
-    copy: "#copy",
-    shortlink: "#shortlink",
-    url: "#url",
-    create: "#create",
-    form: "#shortForm",
-    shortarea: "#short-area",
-    notice: "#copy-notice",
-    share: ".share42init",
-  });
-});
-
-window.openModal = function (modalId) {
-  document.getElementById(modalId).style.display = "block";
-  document.getElementsByTagName("body")[0].classList.add("overflow-y-hidden");
-};
-
-window.closeModal = function (modalId) {
-  document.getElementById(modalId).style.display = "none";
-  document
-    .getElementsByTagName("body")[0]
-    .classList.remove("overflow-y-hidden");
-};
-
-// Close all modals when press ESC
-document.onkeydown = function (event) {
-  event = event || window.event;
-  if (event.keyCode === 27) {
-    document
-      .getElementsByTagName("body")[0]
-      .classList.remove("overflow-y-hidden");
-    let modals = document.getElementsByClassName("modal");
-    Array.prototype.slice.call(modals).forEach((i) => {
-      i.style.display = "none";
-    });
-  }
-};
+  );
+}
+function copyToClipboard2() {
+  const copyAlert2 = document.getElementById("copyAlert2");
+  // Get the text field
+  const shortLink = document.getElementById("shortlink");
+  // Select the text field
+  shortLink.select();
+  shortLink.setSelectionRange(0, 99999); // For mobile devices
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(shortLink.value).then(
+    function () {
+      copyAlert2.classList.remove("hidden");
+      setTimeout(() => {
+        copyAlert2.classList.add("hidden");
+      }, 1000);
+    },
+    function (err) {
+      console.error("Could not copy text: ", err);
+    }
+  );
+}
